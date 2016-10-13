ebd55b1e2e955b0da486255c0fe6c583c83e528b
Supakan Siwatmongkol <supakansiw@7eleven.cp.co.th>
2016-10-13 17:50:39 +0700

    create TableComponent for call from index

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [
@@ -11,8 +12,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    TableComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent, TableComponent ]
 })
 export class AppModule { }
---
+++ app/table/table.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+	
+@Component({
+  selector: 'my-table',
+  templateUrl: 'app/table/table.component.html'
+})
+export class TableComponent{
+	
+}
